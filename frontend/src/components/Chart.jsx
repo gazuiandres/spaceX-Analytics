fc3bbf274753515ba552db804c4d65b541a61ac2
= <=>
2023-03-03 21:03:37 -0500

    adding Chart component

---
+++ frontend/src/components/Chart.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BarChart, XAxis, YAxis, Bar, ResponsiveContainer, Tooltip, LabelList } from "recharts";
+
+const Chart = ({ data, nameKey, totalKey }) => {
+    console.log('chart', data)
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+        <BarChart data={data}>
+            <XAxis stroke="white" dataKey={nameKey} />
+            <YAxis type="number" stroke="white" domain={[0, 'dataMax+10']} />
+            <Tooltip />
+            <Bar dataKey={totalKey} fill="#DD2A2A">
+                <LabelList dataKey={totalKey} fill="white" position="top" />
+            </Bar>
+        </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default Chart;
